927f6232584259c8d581a6fcbd5349da0ca23b57
Kira <andrey.cheban576@gmail.com>
2024-09-14 17:32:06 +0300

    Попытка написания цветового круга на WPF

---
+++ MonitorBacklightingWPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MonitorBacklightingWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Thickness margin = ColorPep.Margin;
+            margin.Left += 5 ;
+            margin.Right -= 5;
+            margin.Top += 6;
+            margin.Bottom -= 6;
+            this.ColorPep.BeginAnimation(FrameworkElement.MarginProperty, new ThicknessAnimation(margin, TimeSpan.FromSeconds(0.10)));
+            
+        }
+    }
+}
